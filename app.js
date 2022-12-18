d7134741b31a394cd1a48d2b61df1b0a52f9f22d
bjornclauw <32811735+bjornclauw@users.noreply.github.com>
2022-12-18 15:54:21 +0100

    1st commit

--- app.js
+++ app.js
@@ -6,10 +6,7 @@ var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var moment       = require('moment');
-<<<<<<< Updated upstream
-=======
 require('moment/locale/nl');
->>>>>>> Stashed changes
 const createSessionMiddleware = require('./lib/middleware/withSession');
 
 var app = express();
