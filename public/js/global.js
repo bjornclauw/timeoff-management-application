7a5c8d7bc0ec579813df6c6767edcdc080967b5c
bjornclauw <32811735+bjornclauw@users.noreply.github.com>
2022-12-18 03:57:56 +0100

    Revert "Revert "bug""
    This reverts commit eefb0f0bb74ac2e4f83c6c495712d9edb7c7f254.

--- public/js/global.js
+++ public/js/global.js
@@ -34,23 +34,23 @@ $(document).ready(function(){
 !function(a){
   a.fn.datepicker.dates["en-GB"] = {
     days : [
-      "Zondag","Maandag","Dinsdag","Woensdag","Donderdag","Vrijdag","Zaterdag"
+      "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"
     ],
     daysShort : [
-      "Zon","Maa","Din","Woe","Don","Vri","Zat"
+      "Sun","Mon","Tue","Wed","Thu","Fri","Sat"
     ],
     daysMin : [
-      "Zo","Ma","Di","Wo","Do","Vr","Za"
+      "Su","Mo","Tu","We","Th","Fr","Sa"
     ],
     months : [
-      "Januari","Februari","Maart","April","Mei","Juni","Juli","Augustus","September","Oktober","November","December"
+      "January","February","March","April","May","June","July","August","September","October","November","December"
     ],
     monthsShort : [
-      "Jan","Feb","Maa","Apr","Mei","Jun","Jul","Aug","Sep","Okt","Nov","Dec"
+      "Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"
     ],
-    today       : "Vandaag",
-    monthsTitle : "Maanden",
-    clear       : "Wis",
+    today       : "Today",
+    monthsTitle : "Months",
+    clear       : "Clear",
     weekStart   : 1,
     format      : "dd/mm/yyyy"
   }
@@ -171,7 +171,7 @@ $(document).ready(function(){
 
 $(document).ready(function(){
   $('.user-details-summary-trigger').popover({
-    title: 'Samenvatting werknemer',
+    title: 'Employee summary',
     html: true,
     trigger: 'hover',
     placement: 'auto',
@@ -196,7 +196,7 @@ $(document).ready(function(){
 
 $(document).ready(function(){
   $('.leave-details-summary-trigger').popover({
-    title: 'Samenvatting',
+    title: 'Leave summary',
     html: true,
     trigger: 'hover',
     placement: 'auto',
@@ -238,7 +238,7 @@ $(document).ready(function() {
           if (!data || !data.length) {
             badge.addClass('hidden');
             dropDown.empty();
-            dropDown.append('<li class="dropdown-header">Geen meldingen</li>')
+            dropDown.append('<li class="dropdown-header">No notifications</li>')
 
             document.title = document.title.replace(/\(\d+\)\s*/, '');
 
